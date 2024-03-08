d60ffdc7d0cc76465786037d178dece9ddba0197
Picafres4 <al02921139@tecmilenio.mx>
2024-03-07 20:51:45 -0600

    Initial Commit

---
+++ ApplePie/Game.swift
@@ -0,0 +1,39 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by athaína rada on 07/03/24.
+//
+
+import Foundation
+
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+        var formattedWord: String {
+            var guessedWord = ""
+            for letter in word {
+                if guessedLetters.contains(letter) {
+                    guessedWord += "\(letter)"
+                } else {
+                    guessedWord += "_"
+                }
+            }
+            return guessedWord
+        }
+        
+
+    }
+    
+
+
+
